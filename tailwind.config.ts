cd3917800808ffb68ff0b59965568f164ca57ba7
Brodi White <brodibranded@gmail.com>
2025-06-05 01:59:08 +0000

    Lets introduce breakpoints for medium, large, and extra large

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,13 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
       fontFamily: {
         body: ['var(--font-lato)', 'sans-serif'],
         headline: ['var(--font-playfair-display)', 'serif'],
